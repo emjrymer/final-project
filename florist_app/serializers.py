f3a915653702202fa224247dc2968ae790278e61
emjwivell <emjwivell@gmail.com>
2016-04-06 19:33:22 -0400

    Updating to master

--- florist_app/serializers.py
+++ florist_app/serializers.py
@@ -32,3 +32,14 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
+        fields = ('name', 'email', 'username', 'password')
+
+    def create(self, validated_data):
+        user = User(
+            name=validated_data['name'],
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
