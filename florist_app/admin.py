b3ebce28d6d63db7f9e45550fb96eba34542ef7c
emjwivell <emjwivell@gmail.com>
2016-04-14 10:12:31 -0400

    correction to admin reg

--- florist_app/admin.py
+++ florist_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from florist_app.models import Arrangement, Basket, Florist, Buyer
+from florist_app.models import Arrangement, Cart, Florist, Buyer
 
-admin.site.register([Arrangement, Basket, Florist, Buyer])
+admin.site.register([Arrangement, Cart, Florist, Buyer])
